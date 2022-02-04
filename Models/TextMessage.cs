b86652f42c37dabf0f4f27b7e1cea0221af43e1d
Etienne Hurand <informatique@etiennehurand.fr>
2022-02-04 10:58:00 +0100

    Initial commit

---
+++ Models/TextMessage.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunityToolkitMvvmHelloworld.Models
+{
+    public class TextMessage : ObservableObject
+    {
+        private string text = "Original text";
+        public string Content
+        {
+            get
+            {
+                return text;
+            }
+
+            set
+            {
+                text = value;
+                OnPropertyChanged("Content");
+            }
+        }
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkitMvvmHelloworld.Models;
+
+namespace CommunityToolkitMvvmHelloworld.ViewModels
+{
+    public class MainWindowViewModel : ObservableRecipient
+    {
+        private TextMessage currentTextMessage;
+        public TextMessage CurrentTextMessage
+        {
+            get
+            {
+                return currentTextMessage;
+            }
+
+            set
+            {
+                currentTextMessage = value;
+                OnPropertyChanged("CurrentTextMessage");
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            currentTextMessage = new TextMessage();
+            ChangeTextMessageCommand = new
+                RelayCommand(ChangeTextMessageMethod);
+        }
+
+        private void ChangeTextMessageMethod()
+        {
+            CurrentTextMessage.Content = "Message modified !";
+
+        }
+
+        private void ChangeTextProvider()
+        {
+            ChangeTextMessageMethod();
+        }
+
+        public ICommand ChangeTextMessageCommand { get; }
+    }
+}
